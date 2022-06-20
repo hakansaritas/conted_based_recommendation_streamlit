0b02f9346b27af17b06cd787da71c8612b7a1fb8
HAKAN SARITAŞ <89347460+hakansaritas@users.noreply.github.com>
2022-06-20 14:02:38 +0300

    Update 3_📊_LDA_topic_map.py

--- pages/3_📊_LDA_topic_map.py
+++ pages/3_📊_LDA_topic_map.py
@@ -8,8 +8,9 @@ st.write("If you enter a high alpha, then you’ll distribute each document "
          "The advantage with high alpha is that documents seem to be more similar,"
          " while if you have specialized documents, then a low alpha will keep them"
          " divided into few topics (Falk 2019)")
-
-HtmlFile = open("./pages/lda.html", 'r', encoding='utf-8')
-source_code = HtmlFile.read()
-print(source_code)
-components.html(source_code,height = 800, width = 1300)
+col1,col2 = st.columns([1,2]):
+with col1():               
+         HtmlFile = open("./pages/lda.html", 'r', encoding='utf-8')
+         source_code = HtmlFile.read()
+         print(source_code)
+         components.html(source_code,height = 800, width = 1300)
