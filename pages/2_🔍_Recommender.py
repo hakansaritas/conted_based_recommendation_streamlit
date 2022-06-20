bd3499cbd0c3a36d3c3580f3ba903dff5717b32b
HAKAN SARITAŞ <89347460+hakansaritas@users.noreply.github.com>
2022-06-20 14:17:54 +0300

    Update 2_🔍_Recommender.py

--- pages/2_🔍_Recommender.py
+++ pages/2_🔍_Recommender.py
@@ -49,7 +49,7 @@ with st.container():
 
         st.subheader("tracks (only 30 sec)")
         for song_url in song_html:
-            components.iframe(song_url, height=50, scrolling=True)
+            components.iframe(song_url, height=50, width =5, scrolling=True)
 
         st.subheader("Info")
         st.write(df_recom)
